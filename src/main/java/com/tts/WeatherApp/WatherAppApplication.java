440d6b4a0c08d32497abfb48f669f3a1f24718f0
Idalia Z. Espino Lira <idalia.espino12@gmail.com>
2022-07-26 16:36:26 -0500

    class work

---
+++ src/main/java/com/tts/WeatherApp/WatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.tts.WeatherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatherAppApplication.class, args);
+	}
+
+}
